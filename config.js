eba0b87863bb7d375ae7ec2910be4dd4658e2f30
polwiker1 <cervezawicca@hotmail.com.ar>
2026-06-09 10:32:42 -0300

    Point mini app to funds receiver presale test

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.WIKER_CONFIG = {
   chainName: "Arbitrum Sepolia",
   rpcUrl: "https://sepolia-rollup.arbitrum.io/rpc",
   blockExplorerUrl: "https://sepolia.arbiscan.io",
-  presaleAddress: "0xf3f2ae12c199b26c1c87607aab765553edf85f73",
+  presaleAddress: "0xb6065b78645c1c308a23777130ad7bd9373df4d7",
   wkrAddress: "0xccbec681b1ead082cac3c5ea55964f1912b45250",
   usdtAddress: "0x36e3De3965c43A4e3A10Fd40d6468EBc910fb953",
   usdcAddress: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d",
